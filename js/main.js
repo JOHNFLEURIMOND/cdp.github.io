06b0e76f5d33baff7abebd08b0659a766ec0b962
JOHNFLEURIMOND <johncfleurimond@gmail.com>
2019-12-10 13:00:40 -0500

    trying to get data on dom. Github pages up

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
 "use strict";
+
 /*Fetching JSON Data
 Author:John Fleurimond
 LIC:
 */
+
 alert("Hi");
-// let json = "../data/CDPstations.json"
-// fetch(json)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(data) {
-//       appendData(data);
-//     })
-//     .catch(function(err) {
-//       console.log(err);
-//     });
+
 async function fetchJSON() {
   alert("Hello Fetching JSON");
-  let json = "../data/CDPstations.json";
-  await fetch(json)
+
+  await fetch("./data/CDPstations.json")
     .then(function(response) {
       return response.json();
     })
@@ -30,11 +22,14 @@ async function fetchJSON() {
     });
   return fetchJSON;
 }
+
 //Loop through every object in our JSON object
 function appendData(data) {
   //Get the div element from the body
+
   alert("Hello");
   console.log("Hi");
+
   var mainContainer = document.getElementById("myData");
   for (var i = 0; i < data.length; i++) {
     var div = document.createElement("div");
@@ -43,6 +38,7 @@ function appendData(data) {
   }
   return appendData;
 }
+
 //                         Moment.js
 var Now = moment().format("MMMM Do YYYY, h:mm:ss a");
 var x = document.getElementById("msgdiv");
