7580678ec1921fac91727ee9e922ca065e194160
JOHNFLEURIMOND <johncfleurimond@gmail.com>
2019-12-09 20:05:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+![Fleurimond]()
+
+
+# My Awesome Project
+
+# How It's Made:
+
+## CSS~
+
+## Javascript~
+
+
+## Optimizations.
+
+
+
+## Lessons Learned:
+
+## portfolio:
+
+**WEBSITE:** https:/johnfleurimond.com
+
+## Installation
+
+1. Clone repo
+2. run `npm install`
+
+## Usage
+
+1. Right click on the `index.html` file in the folder and then right click and select the `Open in Browser` button.
---
+++ js/main.js
@@ -0,0 +1,34 @@
+/*Fetching JSON Data
+Author:John Fleurimond
+LIC:
+*/
+
+alert('Hi');
+
+fetch('./data/CDPstations.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+//Get the div element from the body
+var mainContainer = document.getElementById("myData");
+//Loop through every object in our JSON object
+function appendData(data) {
+  var mainContainer = document.getElementById("myData");
+  for (var i = 0; i < data.length; i++)
+  {
+    var div = document.createElement("div");
+    div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+    mainContainer.appendChild(div);
+  }
+
+  //                         Moment.js
+  var Now = moment().format("MMMM Do YYYY, h:mm:ss a");
+  var x = document.getElementById("msgdiv").innerHTML;
+  x = Now
+}
